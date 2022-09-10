9fb57f52c5311799dbfbc083130e82292ba61222
cemk4zxc <ceme4k4@mail.ru>
2022-09-10 11:35:44 +0300

    Добавьте файлы проекта.

---
+++ HomeWork9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using System.Threading;
+
+namespace HomeWork9
+{
+    class Program
+    {
+        
+    }
+}
